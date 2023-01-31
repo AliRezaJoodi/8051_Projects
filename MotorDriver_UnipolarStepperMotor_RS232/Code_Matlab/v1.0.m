4cffaa83e01cbdd8e28c182ffe1d90626b3ee02c
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-01-31 06:13:26 +0330

    Update

--- MotorDriver_UnipolarStepperMotor_RS232/Code_Matlab/v1.0.m
+++ MotorDriver_UnipolarStepperMotor_RS232/Code_Matlab/v1.0.m
@@ -1,24 +1,20 @@
-%--------------------------------------------------------------------------
+% GitHub Account: GitHub.com/AliRezaJoodi
+
 clc;
 clear;
-%--------------------------------------------------------------------------
-fprintf('**************************************************************\r')
-fprintf('* Title         :  Stepper Motor Driver With Matlab          *\r')
-fprintf('**************************************************************\r')
-fprintf('\r')
-%--------------------------------------------------------------------------
+fprintf('GitHub Account: GitHub.com/AliRezaJoodi\r')
+
 s=serial('COM1');
 set(s,'baudrate',9600,'Terminator',13,'Timeout',1,'InputBufferSize',16,'OutputBufferSize',8);
 fopen(s);
-%--------------------------------------------------------------------------
+
 stepper_data=1;
 while (1)
-    %----------------------------------------------------------------------
     fprintf('\r')
     step=input('How many steps you want to move?  ');
     speed=input('How quickly you move?  ');
     sake=input('Want to move in what direction? L/R  ','S');
-    %----------------------------------------------------------------------
+
     if sake=='L' || sake=='l' ;
         fprintf('Running ... \r')
         for i=1:step
@@ -27,7 +23,7 @@ while (1)
             stepper_data_str=num2str(stepper_data); fprintf(s,stepper_data_str); pause(speed); 
         end
      end
-    %----------------------------------------------------------------------
+
     if sake=='R' || sake=='r' ;
         fprintf('Running ... \r')
         for i=1:step
@@ -37,7 +33,7 @@ while (1)
         end
     end
 end
-%--------------------------------------------------------------------------
+
 clear;
 fclose(s);
 delete(s);
